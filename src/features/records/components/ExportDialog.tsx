7975fef9835352d7d9771265695150d01f9ad235
rivas-kdt <rivas-kdt@global.kawasaki.com>
2026-04-14 10:20:27 +0800

    Export fix

--- src/features/records/components/ExportDialog.tsx
+++ src/features/records/components/ExportDialog.tsx
@@ -17,12 +17,13 @@ import {
 } from "@/components/ui/select";
 import { FileSpreadsheet } from "lucide-react";
 import { useTranslations } from "next-intl";
+import { useState } from "react";
 
 type Props = {
   open: boolean;
   onOpenChange: (v: boolean) => void;
   onExportAll: () => void;
-  onExportMonth: () => void;
+  onExportMonth: (month: number) => void;
   onExportSelected: () => void;
 };
 
@@ -34,10 +35,12 @@ export function ExportDialog({
   onExportSelected,
 }: Props) {
   const t = useTranslations("modals");
+  const [month, setMonth] = useState(0);
+  console.log("Selected month:", month);
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="box-design max-w-6xl focus:outline-none">
-        <DialogHeader className="flex flex-col justify-between pb-2 border-b-1 border-primary">
+        <DialogHeader className="flex flex-col justify-between pb-2 border-b border-primary">
           <DialogTitle className="flex items-center gap-2 text-black-text">
             <FileSpreadsheet className="w-7 h-7" />
             {t("exportData")}
@@ -51,9 +54,11 @@ export function ExportDialog({
           onImportSuccess={() => onOpenChange(false)}
         /> */}
 
-        <div className="flex flex-row gap-5 justify-between max-h-[300px]">
-          <DialogHeader className="flex flex-col gap-5 border-1 border-gray-500 w-[350px] p-6 rounded-lg">
-            <DialogTitle className="font-bold">{t("exportAllData")}</DialogTitle>
+        <div className="flex flex-row gap-5 justify-between max-h-75">
+          <DialogHeader className="flex flex-col gap-5 border border-gray-500 w-87.5 p-6 rounded-lg">
+            <DialogTitle className="font-bold">
+              {t("exportAllData")}
+            </DialogTitle>
             <DialogDescription className="text-black-text">
               {t("exportAllDataDesc")}
             </DialogDescription>
@@ -66,20 +71,21 @@ export function ExportDialog({
             </button>
           </DialogHeader>
 
-          <DialogHeader className="flex flex-col gap-5 border-1 border-gray-500 w-[350px] p-6 rounded-lg">
-            <DialogTitle className="font-bold">
-              {t("exportMonth")}
-            </DialogTitle>
+          <DialogHeader className="flex flex-col gap-5 border border-gray-500 w-87.5 p-6 rounded-lg">
+            <DialogTitle className="font-bold">{t("exportMonth")}</DialogTitle>
             <DialogDescription className="text-black-text mb-10">
               {t("exportMonthDesc")}
             </DialogDescription>
 
             <div className="flex flex-row justify-between gap-2 mt-auto">
-              <Select>
-                <SelectTrigger className="border-1 border-gray-300 rounded-md text-black-text px-3 py-5 w-full">
+              <Select
+                onValueChange={(value) => setMonth(Number(value))}
+                value={String(month)}
+              >
+                <SelectTrigger className="border border-gray-300 rounded-md text-black-text px-3 py-5 w-full">
                   <SelectValue placeholder={t("selectMonth")} />
                 </SelectTrigger>
-                <SelectContent className="h-[200px] bg-white text-black border-gray-300">
+                <SelectContent className="h-50 bg-white text-black border-gray-300">
                   {[
                     "January",
                     "February",
@@ -93,8 +99,12 @@ export function ExportDialog({
                     "October",
                     "November",
                     "December",
-                  ].map((m) => (
-                    <SelectItem key={m} value={m} className="selection-hover">
+                  ].map((m, index) => (
+                    <SelectItem
+                      key={m}
+                      value={String(index)}
+                      className="selection-hover"
+                    >
                       {m}
                     </SelectItem>
                   ))}
@@ -103,15 +113,17 @@ export function ExportDialog({
 
               <button
                 className="gradient-bg rounded-md text-white px-5 py-2 w-full text-nowrap cursor-pointer"
-                onClick={onExportMonth}
+                onClick={() => onExportMonth(month)}
               >
                 {t("exportThisMonth")}
               </button>
             </div>
           </DialogHeader>
 
-          <DialogHeader className="flex flex-col gap-5 border-1 border-gray-500 w-[350px] p-6 rounded-lg">
-            <DialogTitle className="font-bold">{t("exportCheckedData")}</DialogTitle>
+          <DialogHeader className="flex flex-col gap-5 border border-gray-500 w-87.5 p-6 rounded-lg">
+            <DialogTitle className="font-bold">
+              {t("exportCheckedData")}
+            </DialogTitle>
             <DialogDescription className="text-black-text">
               {t("exportCheckedDataDesc")}
             </DialogDescription>
